f751c46a79361f925d350b977b67ff174edc7ade
saileej <99574730+saileej@users.noreply.github.com>
2022-05-08 20:51:48 -0500

    UPDATED

---
+++ Earthquake_Challenge/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoic2FpbGVlaiIsImEiOiJjbDJ4dXhzemswYXEyM2ltbGdodXBkamN6In0.2A9QaaoXGwgU1LET66sYrA";
---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,34 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([36.6013, -90.3790], 5);
+
+let line = [
+    
+    [37.615983, -122.392477],
+    [30.1974292, -97.6663058],
+    [43.6777176, -79.6248196],
+    [40.641766, -73.780968]
+    
+];
+
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    dashArray: '20,15',
+    color: "blue"
+  }).addTo(map);
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+
+//We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
